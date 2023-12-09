083a5f372b01da396122ce63645cd74e62fd86ff
Hz6826 <hz_2007@qq.com>
2023-12-09 15:56:29 +0800

    Initial commit

---
+++ pychat/init.py
@@ -0,0 +1,20 @@
+import sys, os
+
+try:
+    from beta.source.custom import *
+except ImportError:
+    sys.path = [os.path.dirname(sys.argv[0])] + sys.path
+    from source.custom import *
+os.chdir(os.path.dirname(__file__))
+from .chatapi import *
+
+
+class AddonPage(BasicPage):
+    """
+    插件主页面
+    """
+    title = "PyChat"
+    subtitle = "聊天"
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setIcon(FIF.CHAT)
